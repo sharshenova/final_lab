6e495ccffd0d6a7983181392cea1ec3c3072a3d4
Maria Sharshenova <maria@sharshenov.com>
2019-06-28 01:51:52 +0600

    Register urls

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from webapp.models import Article
+from webapp.models import Article, Comment
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['pk', 'title', 'author', 'created_at']
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'text', 'author', 'created_at']
+
+
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView, \
+    CommentCreateView, CommentUpdateView, CommentDeleteView
 
 app_name = 'webapp'
 
@@ -8,4 +9,9 @@ urlpatterns = [
     path('articles/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('articles/create/', ArticleCreateView.as_view(), name='article_create'),
     path('articles/<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_update'),
+    path('articles/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('articles/<int:pk>/addcomment', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/edit', CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete', CommentDeleteView.as_view(), name='comment_delete'),
+
 ]
